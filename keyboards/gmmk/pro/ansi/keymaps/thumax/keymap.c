3a63d186ab67d9210fae2fdbafcb712974a7a455
thumax <38001267+thumax@users.noreply.github.com>
2022-04-10 20:13:19 +0200

    Added media controls to encoder.

--- keyboards/gmmk/pro/ansi/keymaps/thumax/keymap.c
+++ keyboards/gmmk/pro/ansi/keymaps/thumax/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [META] = LAYOUT(
-        XXXXXXX, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_INS,           XXXXXXX,
+        XXXXXXX, KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24,  KC_INS,           KC_MPLY,
         XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, KC_DEL,           KC_NUM,
         XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, KC_CAPS,          KC_PSLS,
         _______, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_DEL,           KC_KP_ENTER,      KC_PAST,
@@ -50,6 +50,12 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
             rgblight_increase_hue_noeeprom();
         } else {
             rgblight_decrease_hue_noeeprom();
+        }
+    } else if (IS_LAYER_ON(META)) {
+        if (clockwise) {
+            tap_code16(KC_MNXT);
+        } else {
+            tap_code16(KC_MPRV);
         }
     } else {
         if (get_mods() & MOD_BIT(KC_LCTRL)) {
