9512a5ae83d2f6323ffbbd6754036aade380d209
thumax <38001267+thumax@users.noreply.github.com>
2022-06-05 22:57:52 +0200

    Meta arrow keys to NUM.

--- keyboards/gmmk/pro/rev1/ansi/keymaps/thumax/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/thumax/keymap.c
@@ -29,8 +29,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7,   KC_P8,   KC_P9,   KC_P0,   KC_PMNS, KC_PPLS, KC_DEL,           KC_NUM,
         XXXXXXX, KC_VOLD, KC_MUTE, KC_VOLU, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, KC_CAPS,          KC_PSLS,
         _______, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_INS,  KC_DEL,           KC_KP_ENTER,      KC_PAST,
-        _______,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          _______, KC_F14,  KC_PDOT,
-        _______, KC_APP,  _______,                            KC_LCTL,                            MO(UTIL),_______, _______, KC_MPRV, KC_MPLY, KC_MNXT
+        _______,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_END,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          KC_P5,   KC_P8,   KC_PDOT,
+        _______, KC_APP,  _______,                            KC_LCTL,                            MO(UTIL),_______, _______, KC_P4,   KC_P2,   KC_P6
     ),
 
     [UTIL] = LAYOUT(
